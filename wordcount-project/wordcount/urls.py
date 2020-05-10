301bbaceb6adcf2e20ddb30951538f3233c3bbf3
avinpereira <avinpereira@gmail.com>
2020-05-10 14:04:41 +0530

    Simple Word Counter Website Completed

--- wordcount-project/wordcount/urls.py
+++ wordcount-project/wordcount/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path('',home),
-    path('admin/', admin.site.urls),
+    path('',views.home, name='home'),
+    path('about/',views.about, name='about'),
+    path('count/',views.count, name='count')
 ]
--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,4 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
+
 def home(request):
     return render(request, 'home.html')
+
+
+def count(request):
+    full_text = request.GET['fulltext']
+    word_list = full_text.split()
+    word_dict = {}
+    for word in word_list:
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+
+    sorted_words = sorted(word_dict.items(), key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html', {'fulltext':full_text, 'count':len(word_list),'worddictionary':sorted_words})
+
+
+def about(request):
+    return render(request, 'about.html')
